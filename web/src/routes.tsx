9085c163bbbe9a31be5445dc3e603e1f49d28f5c
Gabriel Paiva <gabriel_silvapaiva@hotmail.com>
2020-10-16 07:50:10 -0300

    feat(map): add new improvements to map and add new pages

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import OrphanagesMap from './pages/OrphanagesMap';
+import Orphanage from './pages/Orphanage';
+import CreateOrphanage from './pages/CreateOrphanage';
 
 function Routes () {
   return (
@@ -9,6 +11,8 @@ function Routes () {
       <Switch>
         <Route path="/" exact component={Landing}/>
         <Route path="/app" exact component={OrphanagesMap}/>
+        <Route path="/orphanages/create" exact component={CreateOrphanage}/>
+        <Route path="/orphanages/:id" exact component={Orphanage}/>
       </Switch>    
     </BrowserRouter>
   );
